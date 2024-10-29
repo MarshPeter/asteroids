f19c3da1245c3ff1dd30a9c594e6cd68679f8460
MarshPeter <peter.m.farmer.204@gmail.com>
2024-10-29 21:34:35 +1100

    created Player and grouped it into updatable and drawable containers

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Player.containers = (updatable, drawable)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+        for d in drawable:
+            d.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
